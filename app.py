fbe13b16edbb85daa11eab5ac1f5f49ecc610899
YuNats <yuki.kobayashi1990@gmail.com>
2022-05-30 22:52:27 +0900

    PIのGPIOをドッキング中

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import json
 from flask import Flask, Response, render_template, jsonify, url_for
 import base64
-import glob
-import cv2
-import socket
-import subprocess
+
 from concurrent.futures import ThreadPoolExecutor
 import logging
 import time
+import RPi.GPIO as GPIO
 
 import flask
 
@@ -18,37 +16,31 @@ import flask
 #  * Restarting with stat
 
 app= Flask(__name__)
-
+out_pins=[4,17,27,22,5,6,13,19,26]
 @app.route('/')
 def index():
+    # GPIO番号指定の準備
+    GPIO.setmode(GPIO.BCM)
+
+    # LEDピンを出力に設定
+    for pin in out_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
     return render_template('index.html')
 
-@app.route('/ajax', methods=['GET','POST'])
-def ajax():
-    print('ajax here')
-    files = glob.glob("C:\\Users\\user\\Pictures\\test_picture\\*")
-    cnt=0
-    jsondat={}
-    for file in files:
-        print(file)
-        #ファイルオープン方式（画像が大きいと重い）
-        #with open(file, "rb") as image_file:
-        #    data = base64.b64encode(image_file.read())
+@app.route('/ajax/<int:pin>', methods=['GET','POST'])
+def ajax(pin):
+    target_pin=pin
+    if(target_pin in out_pins):
+        GPIO.output(pin, GPIO.HIGH)
+        return jsonify('0')
+    else:
+        return jsonify('-1')
 
-        #opencv方式(リサイズとか画像処理もできる)
-        img=cv2.imread(file)
-        height, width, channels = img.shape[:3]
-        img=cv2.resize(img, [int(width*0.15), int(height*0.15)])
-        _, encoded = cv2.imencode(".jpg", img)
-        data = base64.b64encode(encoded)
-        #jsondat[file]=data.decode('utf-8')#キーがファイル名になる
-        d={'filename':file, 'image':data.decode('utf-8')}
-        jsondat[cnt]=d
-        cnt+=1
-        #print(jsondat[file])
         
     
-    return jsonify(jsondat)
+
 
 @app.route('/quit', methods=['GET','POST'])
 def quit():
@@ -60,5 +52,5 @@ if __name__ =='__main__':
     #コマンドラインから実行するときに,以下が行われる
     print('from cmd')
     app.run(host='localhost', debug=True)
-
+    GPIO.clear()
     
